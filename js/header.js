523d3cfd74aaf089a4f8cd1ea3e58911f1303a74
Gleb <glebshaevvv@mail.ru>
2023-02-13 23:51:31 +0300

    create project

---
+++ js/header.js
@@ -0,0 +1,35 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () =>{
+    
+    const burgerBtn = document.querySelector('.header__burger');
+    const nav = document.querySelector('.nav');
+    const navList = document.querySelectorAll('.nav__item');
+    const mainHtml = document.querySelector('.main');
+    let countBurger = 0;
+    
+    navList.forEach(item =>{
+        item.addEventListener('click', () =>{
+            renderMenu();
+        });
+    });
+    burgerBtn.addEventListener('click', () =>{
+        renderMenu();
+    });
+    
+    
+    function renderMenu(){
+        if (countBurger == 0){
+            nav.classList.add('visible');
+            burgerBtn.classList.add('active');
+            mainHtml.classList.add('none-visible');
+            countBurger++;
+        } else {
+            nav.classList.remove('visible');
+            burgerBtn.classList.remove('active');
+            mainHtml.classList.remove('none-visible');
+            countBurger--;
+        }
+    }
+    
+    });
